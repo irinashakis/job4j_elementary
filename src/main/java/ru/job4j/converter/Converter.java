63fa4295547a9f11d3a48d2c3145ff3c19815910
irinashakis <shakisi@mail.ru>
2022-08-06 12:50:56 +0300

    0. Что такое тестирование [#159431 #329212]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -20,34 +20,34 @@ public class Converter {
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
-        float in1 = 140;
-        float expected1 = 2;
-        float out1 = Converter.rubleToEuro(in1);
-        boolean passed1 = expected1 == out1;
-        System.out.println("140 rubles are 2. Test result : " + passed1);
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
 
         float rubleE = Converter.euroToRuble(2);
         System.out.println("2 euro are " + rubleE + " ruble.");
-        float in2 = 2;
-        float expected2 = 140;
-        float out2 = Converter.euroToRuble(in2);
-        boolean passed2 = expected2 == out2;
-        System.out.println("2 euro are 140. Test result : " + passed2);
+        in = 2;
+        expected = 140;
+        out = Converter.euroToRuble(in);
+        passed = expected == out;
+        System.out.println("2 euro are 140. Test result : " + passed);
 
         float dollar = Converter.rubleToDollar(120);
         System.out.println("120 rubles are " + dollar + " dollars.");
-        float in3 = 120;
-        float expected3 = 2;
-        float out3 = Converter.rubleToDollar(in3);
-        boolean passed3 = expected3 == out3;
-        System.out.println("120 rubles are 2. Test result : " + passed3);
+        in = 120;
+        expected = 2;
+        out = Converter.rubleToDollar(in);
+        passed = expected == out;
+        System.out.println("120 rubles are 2. Test result : " + passed);
 
         float rubleD = Converter.dollarToRuble(2);
         System.out.println("2 dollars are " + rubleD + " ruble.");
-        float in4 = 2;
-        float expected4 = 120;
-        float out4 = Converter.dollarToRuble(in4);
-        boolean passed4 = expected4 == out4;
-        System.out.println("2 dollars are 120. Test result : " + passed4);
+        in = 2;
+        expected = 120;
+        out = Converter.dollarToRuble(in);
+        passed = expected == out;
+        System.out.println("2 dollars are 120. Test result : " + passed);
     }
 }
