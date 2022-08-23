1d33f6eb3cc322b63b1d3562e8a77644ed535a4d
irinashakis <shakisi@mail.ru>
2022-08-23 19:38:12 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,30 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        if (!name.isEmpty()) {
+            char first = name.charAt(0);
+            if (!Character.isUpperCase(first) && !Character.isDigit(first)) {
+                for (int i = 0; i < name.length(); i++) {
+                    int code = name.codePointAt(i);
+                    if (isSpecialSymbol(code) || isUpperLatinLetter(code) || isLowerLatinLetter(code) || Character.isDigit(code)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+        }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
